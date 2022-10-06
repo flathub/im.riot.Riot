7b253f28e47be7a1da3da32c10bd061d246992da
Gaël Donval <gdonval+github@gmail.com>
2022-10-06 17:10:21 +0100

    Update element.sh to add wayland support
    Adds support for wayland by adding the correct command line arguments.

--- element.sh
+++ element.sh
@@ -4,7 +4,11 @@ FLAGS=''
 
 if [[ $XDG_SESSION_TYPE == "wayland" ]] && [ -c /dev/nvidia0 ]
 then
-    FLAGS="$FLAGS --disable-gpu-sandbox"
+    if [ -c /dev/nvidia0 ]
+    then
+        FLAGS="$FLAGS --disable-gpu-sandbox"
+    fi
+    FLAGS="$FLAGS --enable-features=UseOzonePlatform,WaylandWindowDecorations,WebRTCPipeWireCapturer --ozone-platform=wayland"
 fi
 
 env TMPDIR="$XDG_RUNTIME_DIR/app/${FLATPAK_ID:-im.riot.Riot}" zypak-wrapper /app/Element/element-desktop $FLAGS "$@"
